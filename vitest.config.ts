eeccec97ba1107be06862b52a2431da8df21ab0f
reizt <reizt.dev@gmail.com>
2023-07-14 17:37:50 +0900

    Add vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      provider: 'c8',
+    }
   },
   resolve: {
     alias: {
